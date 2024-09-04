296073d8c1dead8d6ad03bfb7605428fad9748e6
ajramirez1997 <ramirezjralfredo1997@gmail.com>
2024-09-05 02:34:22 +0800

    config the vite and nodejs

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
+    plugins: [        
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/js/app.js',            
             ],
-            refresh: true,
+            refresh: true,        
         }),
-    ],
+    ],    
+    build: {
+        manifest: true,        
+        outDir: 'public/build',
+    },
 });
